9c696112edcd9a95e960fb6ce73e9a1111dc3c14
Prof. GuiTozo <125224704+profegui@users.noreply.github.com>
2025-03-13 19:26:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ document.addEventListener('DOMContentLoaded', function(){
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
        })
        
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte')
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte')
+    const alternaContraste = document.getElementById('alterna-contraste')
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
@@ -21,6 +22,9 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
+    })
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
     })
 
 })
